dfe6d47063a2c68c84b6b762caeac8903f2196b1
Andrew Chan Mak <andrew.mak@easynvest.com.br>
2020-02-18 16:52:52 -0300

    ajustando as rotas com auth

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -7,6 +7,7 @@ const stockController = require('./controllers/stockController');
 const AuthController = require("./controllers/AuthController");
 
 const authMiddleware = require("./middlewares/auth");
+//middleware de perfil de usuario
 const aclMiddleware = require("./middlewares/acl");
 
 const routes = express.Router();
@@ -23,15 +24,15 @@ routes.post("/auth/reset_password", AuthController.resetPassword);
 /**
  * Project routes
  */
-routes.post('/devs/:devId/likes', likeController.store);
-routes.post('/devs/:devId/deslikes', deslikeController.store);
-routes.get('/devs', deslikeController.index);
-routes.delete('/devs', deslikeController.delete);
-routes.post('/devs', postController.store);
-routes.get('/devs/:id', postController.store);
-routes.get('/products/', productController.index);
-routes.get('/stocks/', stockController.index);
-routes.get('/products/:id', productController.getbyid);
+routes.post('/devs/:devId/likes', authMiddleware, likeController.store);
+routes.post('/devs/:devId/deslikes', authMiddleware, deslikeController.store);
+routes.get('/devs', authMiddleware, deslikeController.index);
+routes.delete('/devs', authMiddleware, deslikeController.delete);
+routes.post('/devs', authMiddleware, postController.store);
+routes.get('/devs/:id', authMiddleware, postController.store);
+routes.get('/products/', authMiddleware, productController.index);
+routes.get('/stocks/', authMiddleware, stockController.index);
+routes.get('/products/:id', authMiddleware, productController.getbyid);
 routes.get('/stock/:id', authMiddleware, stockController.getbyid);
 
 module.exports = routes;
