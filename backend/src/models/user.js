b15ed14792adfc1fd712ccd57e274cd3d53d9435
Andrew Chan Mak <andrew.mak@easynvest.com.br>
2020-02-18 15:09:14 -0300

    adicionado autenticacao

---
+++ backend/src/models/user.js
@@ -0,0 +1,50 @@
+const { Schema, model } = require('mongoose');
+
+
+const bcrypt = require("bcryptjs");
+
+const UserSchema = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+    lowercase: true
+  },
+  role: {
+    type: String,
+    required: true,
+    default: "user"
+  },
+  password: {
+    type: String,
+    required: true,
+    select: false
+  },
+  passwordResetToken: {
+    type: String,
+    select: false
+  },
+  passwordResetExpires: {
+    type: Date,
+    select: false
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+UserSchema.pre("save", async function(next) {
+  const hash = await bcrypt.hash(this.password, 10);
+  this.password = hash;
+
+  next();
+});
+
+const User = model("User", UserSchema);
+
+module.exports = User;
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -4,10 +4,25 @@ const likeController = require('./controllers/likeController');
 const deslikeController = require('./controllers/deslikeController');
 const productController = require('./controllers/productController');
 const stockController = require('./controllers/stockController');
+const AuthController = require("./controllers/AuthController");
 
+const authMiddleware = require("./middlewares/auth");
+const aclMiddleware = require("./middlewares/acl");
 
 const routes = express.Router();
 
+/**
+ * Authenticate routes
+ */
+routes.post("/auth/register", AuthController.register);
+routes.post("/auth/authenticate", AuthController.authenticate);
+routes.post("/auth/get_user_by_id", authMiddleware, AuthController.getUserById);
+routes.post("/auth/forgot_password", AuthController.forgotPassword);
+routes.post("/auth/reset_password", AuthController.resetPassword);
+
+/**
+ * Project routes
+ */
 routes.post('/devs/:devId/likes', likeController.store);
 routes.post('/devs/:devId/deslikes', deslikeController.store);
 routes.get('/devs', deslikeController.index);
@@ -17,6 +32,6 @@ routes.get('/devs/:id', postController.store);
 routes.get('/products/', productController.index);
 routes.get('/stocks/', stockController.index);
 routes.get('/products/:id', productController.getbyid);
-routes.get('/stock/:id', stockController.getbyid);
+routes.get('/stock/:id', authMiddleware, stockController.getbyid);
 
 module.exports = routes;
