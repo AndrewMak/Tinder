f59e64406738aa6966fc4fd8a171e5de50853f69
Andrew Chan Mak <andrew.mak@easynvest.com.br>
2020-02-18 23:56:41 -0300

    p

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://devshark.herokuapp.com:3000/'
+    baseURL: 'http://devshark.herokuapp.com:3333'
 })
 
 export default api;
